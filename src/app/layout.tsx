2631f809901e2790b019827122f242de2779d260
Raj Aryan <rajxryn@gmail.com>
2023-12-16 17:58:22 +0530

    metadata changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,56 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "AI Influencer | Created by v0.dev",
-  description: "An AI Powered influencer marketing SaaS website created completely by v0.dev",
+  description:
+    "An AI Powered influencer marketing SaaS website created completely by v0.dev",
+  openGraph: {
+    url: "influencerai.vercel.app",
+    title: "AI Influencer | Created by v0.dev",
+    description:
+      "An AI Powered influencer marketing SaaS website created completely by v0.dev",
+    type: "website",
+    siteName: "AI Influencer | Created by v0.dev",
+    images: [
+      {
+        url: "https://influencerai.vercel.app/thumb.png",
+        width: 1200,
+        height: 630,
+        alt: "AI Influencer | Created by v0.dev",
+      },
+      {
+        url: "https://influencerai.vercel.app/thumb.png",
+        width: 800,
+        height: 600,
+        alt: "AI Influencer | Created by v0.dev",
+      },
+      {
+        url: "https://influencerai.vercel.app/thumb.png",
+        width: 600,
+        height: 600,
+        alt: "AI Influencer | Created by v0.dev",
+      },
+      {
+        url: "https://influencerai.vercel.app/thumb.png",
+        width: 400,
+        height: 400,
+        alt: "AI Influencer | Created by v0.dev",
+      },
+      {
+        url: "https://influencerai.vercel.app/thumb.png",
+        width: 200,
+        height: 200,
+        alt: "AI Influencer | Created by v0.dev",
+      },
+    ],
+  },
+  twitter: {
+    title: "AI Influencer | Created by v0.dev",
+    description:
+      "An AI Powered influencer marketing SaaS website created completely by v0.dev",
+    card: "summary_large_image",
+    creator: "@8rxn",
+    images: ["https://influencerai.vercel.app/thumb.png"],
+  },
 };
 
 export default function RootLayout({
@@ -21,7 +70,18 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
-          <div className="bg-black text-white text-center py-2 text-xs">This is a <Link href="https://v0.dev" target="_blank" rel="noreferrer" className="underline underline-offset-4">v0.dev</Link> generated website</div>
+          <div className="bg-black text-white text-center py-2 text-xs">
+            This is a{" "}
+            <Link
+              href="https://v0.dev"
+              target="_blank"
+              rel="noreferrer"
+              className="underline underline-offset-4"
+            >
+              v0.dev
+            </Link>{" "}
+            generated website
+          </div>
           <Header />
           {children}
           <Footer />
